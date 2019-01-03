967a73f569135094d5f5c3cf11f0d4f0df84e044
Eduardo Francisco <eadwardus@disroot.org>
2019-01-03 12:44:46 -0200

    Make nput operate without buffering when necessary

--- src/ioq/nput.c
+++ src/ioq/nput.c
@@ -6,6 +6,11 @@ c_ioq_nput(Ioq *p, char *s, usize n)
 {
 	if (n > c_arr_avail(p->mb))
 		c_ioq_flush(p);
+
+	while (n > c_arr_avail(p->mb))
+		if (!(n -= (p->op)(p->fd, s, MIN(n, IOQBUFSIZ))))
+			return 0;
+
 	c_mem_cpy(p->mb->p + p->mb->n, s, n);
 	p->mb->n += n;
 	return 0;
