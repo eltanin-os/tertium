d9745b4100aac711717c42fda51fd19fcdc22440
Eduardo Francisco <eadwardus@disroot.org>
2019-02-27 21:37:22 -0300

    Use sys_allrw in fmt_fdflush

--- src/fmt/fdflush.c
+++ src/fmt/fdflush.c
@@ -4,7 +4,8 @@
 int
 c_fmt_fdflush(CFmt *p)
 {
-	(p->op)((uintptr)p->farg, p->mb->p, p->mb->n);
+	size r;
+	r = c_sys_allrw(p->op, (uintptr)p->farg, p->mb->p, p->mb->n);
 	p->mb->n = 0;
-	return 0;
+	return r;
 }
