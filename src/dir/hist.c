9dc1e163e249613ba9dcca538efbdc191d68b209
Eduardo Francisco <eadwardus@disroot.org>
2019-04-06 16:31:51 -0300

    Fix use of null node

--- src/dir/hist.c
+++ src/dir/hist.c
@@ -9,20 +9,22 @@ struct hist {
 int
 c_dir_hist(CStat *st, CNode **hist)
 {
-	struct hist h;
+	struct hist *hp;
+	struct hist  h;
 	CNode *np;
 
 	h.dev = st->st_dev;
 	h.ino = st->st_ino;
 
-	np = *hist;
-	for (;;) {
-		if (((struct hist *)(np->p))->dev == h.dev &&
-		    ((struct hist *)(np->p))->ino == h.ino)
-			return 1;
-		if ((np = np->next) == *hist)
-			break;
-	}
+	if ((np = *hist))
+		for (;;) {
+			np = np->next;
+			hp = np->p;
+			if (hp->dev == h.dev && hp->ino == h.ino)
+				return 1;
+			if (np == *hist)
+				break;
+		}
 
 	if (c_dst_lpush(hist, c_dst_lnew(&h, 1, sizeof(h))) < 0)
 		return -1;
