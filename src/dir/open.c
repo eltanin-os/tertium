2a2862779a43b3a00355096f638b32f9ebb1fdcc
Eduardo Francisco <eadwardus@disroot.org>
2019-04-14 03:19:51 -0300

    Make minor changes

--- src/dir/open.c
+++ src/dir/open.c
@@ -5,18 +5,23 @@ int
 c_dir_open(CDir *dir, char *path, uint opts)
 {
 	CArr arr;
+	size r;
 
 	if ((dir->__dir.fd = c_sys_open(path, C_OREAD|C_OCEXEC, 0)) < 0)
 		return -1;
 
 	c_arr_init(&arr, dir->path, sizeof(dir->path));
-	if (c_arr_cats(&arr, path) < 0)
+
+	if ((r = c_arr_cats(&arr, path)) < 0)
 		return -1;
-	if (path[c_arr_bytes(&arr)-1] != '/')
+
+	if (path[r-1] != '/') {
 		c_arr_cats(&arr, "/");
+		r++;
+	}
 
 	dir->nlen = 0;
-	dir->plen = c_arr_bytes(&arr);
+	dir->plen = r;
 	dir->opts = opts;
 
 	if (c_sys_fstat(&dir->info, dir->__dir.fd) < 0)
