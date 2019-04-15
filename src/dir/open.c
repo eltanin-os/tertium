c8d54773ca8fcb3323927465d96258e73a8f1828
Eduardo Francisco <eadwardus@disroot.org>
2019-04-15 05:09:28 -0300

    Close dir after failing to open

--- src/dir/open.c
+++ src/dir/open.c
@@ -13,15 +13,18 @@ c_dir_open(CDir *dir, char *path, uint opts)
 	dir->len  = c_str_len(path, C_USIZEMAX);
 	dir->opts = opts;
 
-	if (c_sys_fstat(&st, dir->fd) < 0)
+	if (c_sys_fstat(&st, dir->fd) < 0) {
+		c_dir_close(dir);
 		return -1;
-
-	dir->dev = st.st_dev;
+	}
 
 	if (!C_ISDIR(st.st_mode)) {
+		c_dir_close(dir);
 		errno = C_ENOTDIR;
 		return -1;
 	}
+
+	dir->dev = st.st_dev;
 
 	return 0;
 }
