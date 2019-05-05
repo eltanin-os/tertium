fbb626007dd8818cff0c401d165439c7d6ac6fd2
Eduardo Francisco <eadwardus@disroot.org>
2019-05-05 12:26:42 -0300

    Make minor style modification

--- src/dst/qpops.c
+++ src/dst/qpops.c
@@ -6,9 +6,9 @@ c_dst_qpops(CQueue *p, CArr *b)
 {
 	usize len, n;
 
-	n = p->mb.a - p->h;
-
-	if ((len = c_str_len(c_arr_get(&p->mb, p->h, sizeof(uchar)), n)) == n)
+	n   = c_arr_total(&p->mb) - p->h;
+	len = c_str_len(c_arr_get(&p->mb, p->h, sizeof(uchar)), n);
+	if (len == n)
 		len += c_str_len(c_arr_data(&p->mb), p->mb.a);
 
 	return c_dst_qpop(p, b, len, sizeof(uchar));
