c1679cd95de4eadf14171936cc0c1438857f82fd
Eduardo Francisco <eadwardus@disroot.org>
2019-07-15 20:56:48 -0300

    Fix bsearch behaviour

--- src/std/bsearch.c
+++ src/std/bsearch.c
@@ -4,19 +4,20 @@
 void *
 c_std_bsearch(void *k, void *b, usize n, usize m, int (*f)(void *, void *))
 {
-	int    r;
+	int r;
 	uchar *p, *s;
 
 	s = (uchar *)b;
 
 	while (n) {
-		p = s + (n >>= 1) * m;
+		p = s + (n >> 1) * m;
 		if (!(r = f(k, p))) {
 			return p;
 		} else if (r > 0) {
 			s = p + m;
 			n--;
 		}
+		n >>= 1;
 	}
 
 	return nil;
