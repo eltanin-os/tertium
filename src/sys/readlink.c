22ef62c34afa798fa57ea53b1f69d6829c939359
Eduardo Francisco <eadwardus@disroot.org>
2019-09-10 03:10:36 -0300

    Append zero in readlink

--- src/sys/readlink.c
+++ src/sys/readlink.c
@@ -4,5 +4,12 @@
 size
 c_sys_readlink(char *buf, usize n, char *s)
 {
-	return c_sys_call(SYS_readlink, s, buf, n);
+	size r;
+
+	if ((r = c_sys_call(SYS_readlink, s, buf, n - 1)) < 0)
+		return -1;
+
+	buf[r] = 0;
+
+	return r;
 }
