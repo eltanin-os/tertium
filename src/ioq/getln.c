21b0c0bd4410cb06a506c61f00d18ccdec9be2e4
Eduardo Francisco <eadwardus@disroot.org>
2019-09-23 00:23:38 -0300

    Always treat array as dynamic
    The internal memory allocator always know when receives a non-owned
    region, so is safe to do this, also will avoid dynamic arrays to fail
    with ENOMEM.

--- src/ioq/getln.c
+++ src/ioq/getln.c
@@ -19,7 +19,7 @@ c_ioq_getln(ctype_ioq *p, ctype_arr *b)
 		else
 			n = (nl - s) + 1;
 
-		if (c_arr_cat(b, s, n, sizeof(uchar)) < 0)
+		if (c_dyn_cat(b, s, n, sizeof(uchar)) < 0)
 			return -1;
 
 		c_ioq_seek(p, n);
