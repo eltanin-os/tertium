1c06b7fca373751c2cc664ef9a39946d01fb1755
Eduardo Francisco <eadwardus@disroot.org>
2019-10-20 01:58:38 -0300

    Generate better pseudo-random values
    rand_genseed isn't supposed to be called too many times, so it's better
    to operate a little more upon the seed, though there isn't much to be
    done, as the only "random" value that we have is the current time.

--- src/rand/genseed.c
+++ src/rand/genseed.c
@@ -1,7 +1,8 @@
 #include <tertium/cpu.h>
 #include <tertium/std.h>
 
-#define MIX() \
+#define F(x) ((x) * 1493)
+#define AVALANCHE() \
 {\
 	r ^= r << 16; r += r << 13; r ^= r >> 7; \
 	r += r <<  9; r ^= r >>  8; r += r << 2; \
@@ -10,19 +11,17 @@
 char *
 c_rand_genseed(char *s, usize n)
 {
-	ctype_taia now;
+	ctype_time tm;
 	u64int r;
 
-	c_taia_now(&now);
-	r = (u64int)c_taia_approx(&now);
-	r = ((r + c_sys_getpid()) * 1493) + (uintptr)s + (uintptr)&n;
-	MIX();
+	c_sys_call(SYS_clock_gettime, CLOCK_REALTIME, &tm);
+	r = F(F(F(F(F(tm.nsec)) + c_sys_getpid()) + (uintptr)s) + (uintptr)&n);
+	AVALANCHE();
 
 	while (n) {
-		c_uint_64bigpack(s, r);
-		s += 8;
 		n -= 8;
-		r = (r ^ (n << 2)) * 1493;
+		c_uint_64bigpack(s + n, r);
+		r = F(r ^ (n << 2));
 	}
 
 	return s;
