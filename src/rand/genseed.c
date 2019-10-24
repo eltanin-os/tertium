2edc9bbd97c63eb3fca7cf68b4b2331923789ee2
Eduardo Francisco <eadwardus@disroot.org>
2019-10-23 22:02:32 -0300

    Make genseed more expensive

--- src/rand/genseed.c
+++ src/rand/genseed.c
@@ -1,29 +1,34 @@
 #include <tertium/cpu.h>
 #include <tertium/std.h>
 
-#define F(x) ((x) * 1493)
-#define AVALANCHE() \
-{\
-	r ^= r << 16; r += r << 13; r ^= r >> 7; \
-	r += r <<  9; r ^= r >>  8; r += r << 2; \
-}
-
 char *
 c_rand_genseed(char *s, usize n)
 {
+	ctype_hst hs;
 	ctype_taia now;
-	u64int r;
+	int r;
+	char buf[20];
 
 	c_taia_now(&now);
-	r = (u64int)c_taia_approx(&now);
-	r = F(F(F(F(F(r)) + c_sys_getpid()) + (uintptr)s) + (uintptr)&n);
-	AVALANCHE();
+	c_taia_pack(buf, &now);
+	c_uint_32bigpack(buf + C_TAIA_PACK, c_sys_getpid());
+	c_uint_32bigpack(buf + 4 + C_TAIA_PACK, (uintptr)s);
+	c_uint_32bigpack(buf + 8 + C_TAIA_PACK, (uintptr)&n);
+	c_hsh_all(&hs, c_hsh_edf, buf, sizeof(buf));
 
-	while (n) {
-		n -= 8;
-		c_uint_64bigpack(s + n, r);
-		r = F(r ^ (n << 2));
-		AVALANCHE();
+	for (;;) {
+		n -= r = C_MIN(n, 4);
+		c_uint_32bigpack(buf, c_hsh_state0(&hs));
+		c_mem_cpy(s + n, r, buf);
+
+		if (!n)
+			break;
+
+		for (r = 0; r < 5; r++) {
+			c_hsh_edf->update(&hs, buf, sizeof(buf));
+			c_hsh_edf->end(&hs);
+			c_uint_32bigpack(buf + (r << 2), c_hsh_state0(&hs));
+		}
 	}
 
 	return s;
