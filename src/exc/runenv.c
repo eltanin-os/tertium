34e61d5ba2f73c5fd3e65ad251eb097d92fa9204
Eduardo Francisco <eadwardus@disroot.org>
2019-11-30 14:10:06 -0300

    Fix assignment to nil region

--- src/exc/runenv.c
+++ src/exc/runenv.c
@@ -21,7 +21,8 @@ c_exc_runenv(char *prog, char **argv, char **envp)
 			if (c_dyn_cat(&e, &*envp, 1, sizeof(*envp)) < 0)
 				return -1;
 
-	c_mem_set((uchar *)c_arr_data(&e) + c_arr_bytes(&e), sizeof(void *), 0);
+	if (e.n)
+		c_mem_set(e.p + e.n, sizeof(void *), 0);
 
 	if ((path = c_str_chr(prog, C_USIZEMAX, '/')))
 		return c_sys_exec(prog, argv, (char **)c_arr_data(&e));
