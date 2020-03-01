395b5be7897b606b8d7dab1e1d866c6546f4273d
Eduardo Francisco <eadwardus@disroot.org>
2020-03-01 14:47:38 -0300

    Fix std_vtoptr arg usage

--- src/std/vtoptr.c
+++ src/std/vtoptr.c
@@ -2,7 +2,7 @@
 #include <tertium/std.h>
 
 void *
-c_std_vtoptr(char *s, ...)
+c_std_vtoptr_(char *s, ...)
 {
 	ctype_arr arr;
 	int i, n;
@@ -10,16 +10,17 @@ c_std_vtoptr(char *s, ...)
 	va_list ap;
 
 	va_start(ap, s);
-	for (n = 0; va_arg(ap, char *); ++n) ;
+	for (n = 1; va_arg(ap, char *); ++n) ;
 	va_end(ap);
 
 	c_mem_set(&arr, sizeof(arr), 0);
 	if (c_dyn_ready(&arr, n + 1, sizeof(char *)) < 0)
 		return nil;
 
-	va_start(ap, s);
 	av = c_arr_data(&arr);
-	for (i = 0; i <= n; ++i)
+	av[0] = s;
+	va_start(ap, s);
+	for (i = 1; i <= n; ++i)
 		av[i] = va_arg(ap, char *);
 	va_end(ap);
 	return av;
