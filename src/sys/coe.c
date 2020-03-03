5c63fe76aff349b3ebaf48ca54852b14f4e6dd42
Eduardo Francisco <eadwardus@disroot.org>
2020-03-03 17:01:33 -0300

    Add missing sys/port macros

--- src/sys/coe.c
+++ src/sys/coe.c
@@ -8,5 +8,5 @@ c_sys_coe(ctype_fd fd)
 
 	if ((opts = c_sys_fcntl(fd, C_FGETFD)) == (uint)-1)
 		return -1;
-	return c_sys_fcntl(fd, C_FSETFD, opts | C_FDCLOEXEC);
+	return c_sys_fcntl(fd, C_FSETFD, opts | C_FDCEXEC);
 }
