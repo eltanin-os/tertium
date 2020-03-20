d78cc7532deb3d729d4e20dda981e65b125467f7
Eduardo Francisco <eadwardus@disroot.org>
2020-03-20 05:29:15 -0300

    Fix adt_lnew and adt_ltpush

--- src/adt/lnew.c
+++ src/adt/lnew.c
@@ -4,9 +4,11 @@
 ctype_node *
 c_adt_lnew(void *v, usize n)
 {
-	void *p;
+	ctype_node *p;
 
-	if (!(p = c_std_alloc(n, sizeof(uchar))))
+	if (!(p = c_std_alloc(n + sizeof(*p), sizeof(uchar))))
 		return nil;
-	return c_mem_cpy(p, n, v);
+	p->prev = p->next = nil;
+	c_mem_cpy(p->p, n, v);
+	return p;
 }
--- src/adt/ltpush.c
+++ src/adt/ltpush.c
@@ -7,13 +7,14 @@ c_adt_ltpush(ctype_node **sp, ctype_node *p)
 	if (!p)
 		return -1;
 
+	p->prev = nil;
 	if (*sp) {
-		p->next = (*sp)->next;
-		(*sp)->next = ((*sp)->next)->prev = p;
+		if (!(p->next = (*sp)->next)->prev)
+			p->next->prev = p;
+		(*sp)->next = p;
 	} else {
 		*sp = p;
 		p->next = p;
 	}
-
 	return 0;
 }
