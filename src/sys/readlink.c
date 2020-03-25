8e04e41846b7bd0f807b3e12c5547f6e9ed7ef54
Eduardo Francisco <eadwardus@disroot.org>
2020-03-25 00:24:48 -0300

    Fix readlink zero append

--- src/sys/readlink.c
+++ src/sys/readlink.c
@@ -4,6 +4,14 @@
 size
 c_sys_readlink(char *s, char *buf, usize n)
 {
-	buf[n - 1] = 0;
-	return c_std_syscall(SYS_readlink, s, buf, n - 1);
+	size r;
+
+	if ((r = c_std_syscall(SYS_readlink, s, buf, n)) < 0)
+		return -1;
+	if ((usize)r == n) {
+		errno = C_ENAMETOOLONG;
+		return -1;
+	}
+	buf[r] = 0;
+	return r;
 }
