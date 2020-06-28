4b16e1d577b582d7b75bb795c877c1fb78eda07a
Eduardo Francisco <eadwardus@disroot.org>
2020-06-27 21:23:19 -0300

    Sort rand_name char table

--- src/rand/name.c
+++ src/rand/name.c
@@ -1,15 +1,15 @@
 #include <tertium/cpu.h>
 #include <tertium/std.h>
 
-static char tab[] = \
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZghijklmnopqrstuvwxyz-_0123456789abcdef";
+static uchar tab[] = \
+    "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
 
 char *
 c_rand_name(char *s, usize n)
 {
 	s[--n] = 0;
 	c_rand_data(s, n);
-	for (; n; --n)
+	while (n--)
 		s[n] = tab[s[n] & 63];
 	return s;
 }
