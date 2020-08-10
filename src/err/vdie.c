7f18b6c1e37023737781b8600f5d8bf606e42cf6
Eduardo Francisco <eadwardus@disroot.org>
2020-08-10 00:36:47 -0300

    Remove redundancy from c_err_vdie(x)

--- src/err/vdie.c
+++ src/err/vdie.c
@@ -4,11 +4,6 @@
 void
 c_err_vdie(int r, char *fmt, va_list args)
 {
-	c_ioq_fmt(ioq2, "%s: ", c_std_getprogname());
-	if (fmt) {
-		c_ioq_vfmt(ioq2, fmt, args);
-		c_ioq_put(ioq2, ": ");
-	}
-	c_ioq_fmt(ioq2, "%r\n", errno);
+	c_err_vwarn(fmt, args);
 	c_std_exit(r);
 }
