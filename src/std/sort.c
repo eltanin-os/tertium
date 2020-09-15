df4b853818abf6ff476c6b17b56b897093e94f46
Eduardo Francisco <eadwardus@disroot.org>
2020-09-14 21:04:55 -0300

    Refactor and reduce memory usage from std_sort

--- src/std/sort.c
+++ src/std/sort.c
@@ -1,7 +1,19 @@
 #include <tertium/cpu.h>
 #include <tertium/std.h>
 
-static inline void
+#define CMP(a, b, c) \
+((a)->f((a)->p + ((b) * (a)->n), (a)->p + ((c) * (a)->n)))
+#define SWAP(a, b, c) \
+swap((a)->p + ((b) * (a)->n), (a)->p + ((c) * (a)->n), (a)->n)
+
+struct sort {
+	uchar *p;
+	usize n;
+	ctype_cmpfn f;
+};
+
+/* swap routines */
+static void
 swap(uchar *a, uchar *b, usize n)
 {
 	for (; n; --n, ++a, ++b) {
@@ -11,65 +23,81 @@ swap(uchar *a, uchar *b, usize n)
 	}
 }
 
+/* merge routines */
 static void
-isrt(uchar *v, usize m, usize n, ctype_cmpfn f)
+mrg(struct sort *p, uchar *buf, usize l, usize m, usize r)
+{
+	usize n1, n2;
+	usize i, j, k;
+
+	n1 = m - l; n2 = r - m;
+	c_mem_cpy(buf, n1 * p->n, p->p + l * p->n);
+	i = j = 0; k = l;
+	while (i < n1 && j < n2) {
+		if (p->f(buf + i * p->n, p->p + (m + j) * p->n) <= 0) {
+			c_mem_cpy(p->p + k * p->n, p->n, buf + i * p->n);
+			++i;
+		} else {
+			c_mem_cpy(p->p + k * p->n, p->n, p->p + (m + j) * p->n);
+			++j;
+		}
+		++k;
+	}
+	c_mem_cpy(p->p + k * p->n, (n1 - i) * p->n, buf + i * p->n);
+	k += (n1 - i);
+	c_mem_cpy(p->p + k * p->n, (n2 - j) * p->n, p->p + (m + j) * p->n);
+}
+
+/* sort routines */
+static void
+isrt(struct sort *p, usize m)
 {
 	usize i, j;
 
 	for (i = 0; i < m; ++i)
-		for (j = i; j && f(v + (j - 1) * n, v + j * n) > 0; --j)
-			swap(v + j * n, v + (j - 1) * n, n);
+		for (j = i; j && CMP(p, j - 1, j) > 0; --j)
+			SWAP(p, j, j - 1);
 }
 
 static void
-mrg(uchar *p, int l, int m, int r, uchar *v, usize n, ctype_cmpfn f)
+recmsrt(struct sort *p, uchar *buf, usize l, usize r)
 {
-	int idx[2], x;
+	usize m;
 
-	idx[0] = l;
-	idx[1] = m;
-
-	for (; l < r; ++l) {
-		x = !(idx[0] < m &&
-		    (idx[1] >= r || f(v + idx[0] * n, v + idx[1] * n) <= 0));
-		c_mem_cpy(p + l * n, n, v + idx[x] * n);
-		++idx[x];
-	}
+	if ((m = r - l) <= 1)
+		return;
+	m = l + (m >> 1);
+	recmsrt(p, buf, l, m);
+	recmsrt(p, buf, m, r);
+	mrg(p, buf, l, m, r);
 }
 
 static void
-msrt(uchar *v, usize m, usize n, ctype_cmpfn f)
+msrt(struct sort *p, usize m)
 {
-	usize i, j;
-	usize t;
-	uchar *p;
+	uchar *buf;
 
-	if ((t = m * n) <= C_BIOSIZ) {
-		p = __builtin_alloca(t);
-	} else if (!(p = c_std_alloc(m, n))) {
-		isrt(v, m, n, f); /* TODO: fast fallback */
+	if (!(buf = c_std_alloc(m >> 1, p->n))) {
+		isrt(p, m); /* XXX */
 		return;
 	}
-
-	for (i = 1; i < m; i <<= 1) {
-		for (j = 0; j < m; j += i << 1)
-			mrg(p, j, C_MIN(j + i, m), C_MIN(j + (i << 1), m),
-			    v, n, f);
-		c_mem_cpy(v, t, p);
-	}
-	c_std_free(p);
+	recmsrt(p, buf, 0, m);
 }
 
 void
 c_std_sort(void *v, usize m, usize n, ctype_cmpfn f)
 {
+	struct sort srt;
+
 	if (C_OFLW_UM(usize, n, m))
 		return;
 
-	if (m <= 12) {
-		isrt(v, m, n, f);
+	srt = (struct sort){v, n, f};
+
+	if (m <= 16) {
+		isrt(&srt, m);
 		return;
 	}
 
-	msrt(v, m, n, f);
+	msrt(&srt, m);
 }
