9ff81917669b96d59836d5f966509d3a6e3c9f34
Eduardo Francisco <eadwardus@disroot.org>
2021-07-23 15:27:21 -0300

    src/std/fdcat.c: Differ read and write errors

--- src/std/fdcat.c
+++ src/std/fdcat.c
@@ -2,14 +2,14 @@
 #include <tertium/std.h>
 
 ctype_status
-c_std_fdcat(ctype_fd target, ctype_fd source)
+c_std_fdcat(ctype_fd dest, ctype_fd src)
 {
 	size r;
 	char buf[C_BIOSIZ];
 
-	while ((r = c_sys_read(source, buf, sizeof(buf))) > 0)
-		if (c_std_allrw(c_sys_write, target, buf, r) < 0)
-			return -1;
+	while ((r = c_sys_read(src, buf, sizeof(buf))) > 0)
+		if (c_std_allrw(c_sys_write, dest, buf, r) < 0)
+			return -2;
 
 	return r < 0 ? -1 : 0;
 }
