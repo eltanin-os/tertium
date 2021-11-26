d227c204e01087934d6bcf50b19215425bcb2d49
Eduardo Francisco <eadwardus@disroot.org>
2021-11-26 12:19:15 -0300

    src/dyn/ready.c: Postpone copy
    A simple optimization (double-check-copy) of a demented algorithm
    (double-copy-check). In addition the growth factor have been changed
    too.

--- src/dyn/ready.c
+++ src/dyn/ready.c
@@ -1,6 +1,8 @@
 #include <tertium/cpu.h>
 #include <tertium/std.h>
 
+#define GFACT 1.618
+
 ctype_status
 c_dyn_ready(ctype_arr *p, usize m, usize n)
 {
@@ -12,15 +14,19 @@ c_dyn_ready(ctype_arr *p, usize m, usize n)
 		return -1;
 	}
 
-	m *= n;
-	m += n;
-	a = p->a ? p->a : m;
-	while (m > c_arr_avail(p)) {
-		a <<= 1;
-		if (!(tmp = c_std_realloc(p->p, a, sizeof(uchar))))
+	m = (m * n) + n;
+	if (p->a) {
+		if (m > c_arr_avail(p)) {
+			a = p->a;
+			while (m > (a - p->n)) a *= GFACT;
+			if (!(tmp = c_std_realloc(p->p, a, sizeof(uchar))))
+				return -1;
+			p->p = tmp;
+			p->a = a;
+		}
+	} else {
+		if (!(p->p = c_std_alloc((p->a = m * GFACT), sizeof(uchar))))
 			return -1;
-		p->p = tmp;
-		p->a = a;
 	}
 	return 0;
 }
