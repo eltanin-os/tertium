abca4e39c3a37f16ad77a9ed7c9dec8419a9f9a5
Eduardo Francisco <eadwardus@disroot.org>
2022-04-07 21:39:29 -0300

    src/mem/set.c: Stop using inline asm

--- src/mem/set.c
+++ src/mem/set.c
@@ -1,15 +1,20 @@
 #include <tertium/cpu.h>
 #include <tertium/std.h>
 
+static void *
+copy(void *v, usize n, int c)
+{
+	uchar *s;
+	s = v;
+	for (; n; --n) *s++ = c;
+	return v;
+}
+
+/* Try to avoid compiler optimization */
+static void *(* volatile set)(void *, usize, int) = copy;
+
 void *
 c_mem_set(void *v, usize n, int c)
 {
-	uchar *s;
-
-	s = v;
-	for (; n; --n)
-		*s++ = c;
-
-	__asm__ __volatile__(""::"r"(v):"memory");
-	return v;
+	return set(v, n, c);
 }
