ebfbd74fbbb17412bf4b326daecd44899dbef78d
Eduardo Francisco <eadwardus@disroot.org>
2022-06-07 01:30:53 -0300

    src/std/atexit.c: Fix function store
    Preserve the original pointer instead of copying it.

--- src/std/atexit.c
+++ src/std/atexit.c
@@ -7,7 +7,8 @@ ctype_status
 c_std_atexit(void (*func)(void))
 {
 	ctype_node *p;
-	p = c_adt_lnew((void *)(uintptr)func, sizeof(func));
-	if (c_adt_lpush(&_tertium_atexit, p) < 0) return -1;
+	if (!(p = c_adt_lnew(nil, 0))) return -1;
+	p->p = (void *)(uintptr)func;
+	c_adt_lpush(&_tertium_atexit, p);
 	return 0;
 }
