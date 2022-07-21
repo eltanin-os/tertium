13c96d8a10341214b241b7d4363e0f433b815bd1
Eduardo Francisco <eadwardus@disroot.org>
2022-07-21 12:22:08 -0300

    src/fmt/verb.c: Fix number verbs

--- src/fmt/verb.c
+++ src/fmt/verb.c
@@ -42,7 +42,6 @@ Vchar(ctype_fmt *p)
 {
 	ctype_rune r;
 	char buf[8];
-
 	r = va_arg(p->args, ctype_rune);
 	c_mem_cpy(buf, sizeof(r), &r);
 	return c_fmt_nput(p, buf, c_utf8_chartorune(&r, buf));
@@ -53,12 +52,12 @@ Verr(ctype_fmt *p)
 {
 	ctype_error errnum;
 	char buf[C_IOQ_ERRSIZ];
-
-	if ((errnum = va_arg(p->args, ctype_error)) == C_ERR_ECSTM)
+	errnum = va_arg(p->args, ctype_error);
+	if (errnum == C_ERR_ECSTM) {
 		c_std_errstr(buf, sizeof(buf));
-	else
+	} else {
 		c_std_strerror(errnum, buf, sizeof(buf));
-
+	}
 	return c_fmt_put(p, buf);
 }
 
@@ -85,19 +84,16 @@ Vflag(ctype_fmt *p)
 		p->flags |= C_FMT_OUNSIGNED;
 		break;
 	case 'h':
-		if (p->flags & C_FMT_OSHORT)
-			p->flags |= C_FMT_OBYTE;
+		if (p->flags & C_FMT_OSHORT) p->flags |= C_FMT_OBYTE;
 		p->flags |= C_FMT_OSHORT;
 		break;
 	case 'l':
-		if (p->flags & C_FMT_OLONG)
-			p->flags |= C_FMT_OVLONG;
+		if (p->flags & C_FMT_OLONG) p->flags |= C_FMT_OVLONG;
 		p->flags |= C_FMT_OLONG;
 		break;
 	case 'z':
 		p->flags |= C_FMT_OLONG;
-		if (sizeof(uintptr) == sizeof(uvlong))
-			p->flags |= C_FMT_OVLONG;
+		if (sizeof(uintptr) == sizeof(uvlong)) p->flags |= C_FMT_OVLONG;
 		break;
 	}
 	return 1;
@@ -107,58 +103,74 @@ static ctype_status
 Vint(ctype_fmt *p)
 {
 	uvlong l;
-	int b, d, i, j, u, n;
+	uint d, u;
+	int b, i, n;
 	char buf[64];
+	char *s;
+
+	if ((b = getbase(p->r)) == 16) p->flags |= C_FMT_OUNSIGNED;
+	u = p->flags & C_FMT_OUNSIGNED;
 
 	n = 0;
-	l = va_arg(p->args, uvlong);
-
-	if (!(p->flags & C_FMT_OUNSIGNED) && (vlong)l < 0) {
-		++n;
-		l = -(vlong)l;
+	if (p->flags & C_FMT_OVLONG) {
+		l = va_arg(p->args, uvlong);
+		if (!u && (n = (vlong)l < 0)) l = -(vlong)l;
+	} else if (p->flags & C_FMT_OLONG) {
+		l = va_arg(p->args, ulong);
+		if (!u && (n = (long)l < 0)) l = -(long)l;
+	} else if (p->flags & C_FMT_OSHORT) {
+		l = va_arg(p->args, uint);
+		if (!u && (n = (short)l < 0)) l = -(short)l;
+	} else if (p->flags & C_FMT_OBYTE) {
+		l = va_arg(p->args, uint);
+		if (!u && (n = (char)l < 0)) l = -(char)l;
+	} else {
+		l = va_arg(p->args, uint);
+		if (!u && (n = (int)l < 0)) l = -(int)l;
 	}
 
-	b = getbase(p->r);
 	u = (p->r == 'X') ? 'A' : 'a';
-	i = sizeof(buf) - 1;
-	j = 0;
-
-	if (!l)
-		buf[--i] = '0';
-
-	for (; l; ++j) {
-		d = (l % b);
-		if ((p->flags & C_FMT_OCOMMA) && j % 4 == 3) {
-			buf[--i] = ',';
-			++j;
+	s = buf + sizeof(buf);
+	if (!l) {
+		*--s = '0';
+	} else {
+		i = 0;
+		for (; l; ++i) {
+			d = l % b;
+			if ((p->flags & C_FMT_OCOMMA) && i % 4 == 3) {
+				*--s = ',';
+				++i;
+			}
+			*--s = (d < 10) ? d + '0' : u + d - 10;
+			l /= b;
 		}
-		buf[--i] = (d < 10) ? d + '0' : u + d - 10;
-		l /= b;
 	}
 
-	if ((p->flags & C_FMT_OZERO) &&
-	    !(p->flags & (C_FMT_OLEFT | C_FMT_OPREC))) {
-		p->width -= sizeof(buf) - i;
-		for (; p->width >= 0; --p->width)
-			buf[--i] = '0';
+	u = (C_FMT_OZERO|C_FMT_OLEFT|C_FMT_OPREC) & ~p->flags;
+	if (u == (C_FMT_OLEFT|C_FMT_OPREC)) {
+		p->width -= (buf + sizeof(buf)) - s;
+		for (; p->width > 0; --p->width) *--s = '0';
 		p->width = 0;
 	}
 
 	if (p->flags & C_FMT_OSHARP) {
-		if (b == 16)
-			buf[--i] = u + 23;	/* 'x' */
-		if (b == 16 || b == 8)
-			buf[--i] = '0';
+		if (b == 16) {
+			*--s = u + 23;
+			*--s = '0';
+		} else if (b == 8) {
+			*--s = '0';
+		}
 	}
 
-	if (n)
-		buf[--i] = '-';
-	else if (p->flags & C_FMT_OSIGN)
-		buf[--i] = '+';
-	else if (p->flags & C_FMT_OSPACE)
-		buf[--i] = ' ';
+	if (n) {
+		*--s = '-';
+	} else if (p->flags & C_FMT_OSIGN) {
+		*--s = '+';
+	} else if (p->flags & C_FMT_OSPACE) {
+		*--s = ' ';
+	}
 
-	return c_fmt_nput(p, buf + i, sizeof(buf) - (i + 1));
+	return c_fmt_nput(p, s, (buf + sizeof(buf)) - s);
 }
 
 static ctype_status
