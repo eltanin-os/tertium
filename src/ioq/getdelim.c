673c6d66c7008fb7a490def0eac41fa904a7d454
Eduardo Francisco <eadwardus@disroot.org>
2022-09-29 11:42:51 -0300

    src/ioq/getdelim.c: Change API

--- src/ioq/getdelim.c
+++ src/ioq/getdelim.c
@@ -2,23 +2,19 @@
 #include <tertium/std.h>
 
 ctype_status
-c_ioq_getdelim(ctype_ioq *p, ctype_arr *b, int delim)
+c_ioq_getdelim(ctype_ioq *p, ctype_arr *b, char *delim)
 {
 	size r;
-	char *s, *nl;
+	char *s, *nl, *tmp;
 
 	nl = nil;
 	while (!nl) {
-		if ((r = c_ioq_feed(p)) <= 0)
-			return r;
-
+		if ((r = c_ioq_feed(p)) <= 0) return r;
 		s = c_ioq_peek(p);
-		if ((nl = c_mem_chr(s, r, delim)))
-			r = (nl - s) + 1;
-
-		if (c_dyn_cat(b, s, r, sizeof(uchar)) < 0)
-			return -1;
-
+		for (tmp = delim; *tmp; ++tmp) {
+			if ((nl = c_mem_chr(s, r, *tmp))) r = (nl - s) + 1;
+		}
+		if (c_dyn_cat(b, s, r, sizeof(uchar)) < 0) return -1;
 		c_ioq_seek(p, r);
 	}
 	return 1;
--- src/ioq/getln.c
+++ src/ioq/getln.c
@@ -4,5 +4,5 @@
 ctype_status
 c_ioq_getln(ctype_ioq *p, ctype_arr *b)
 {
-	return c_ioq_getdelim(p, b, '\n');
+	return c_ioq_getdelim(p, b, "\n");
 }
