a0417a303068a9dd19d6a6cab3d648f18544f154
Eduardo Francisco <eadwardus@disroot.org>
2023-05-23 02:37:45 -0300

    src/hsh/initk: Add padding

--- src/hsh/initk.c
+++ src/hsh/initk.c
@@ -4,5 +4,7 @@
 void
 c_hsh_initk(ctype_hst *p, void *k, usize n)
 {
+	if (n > sizeof(p->buf)) n = sizeof(p->buf); /* XXX */
 	c_mem_cpy(p->buf, n, k);
+	c_mem_set(p->buf + n, sizeof(p->buf) - n, 0);
 }
