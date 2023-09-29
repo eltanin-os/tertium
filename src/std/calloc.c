d06c0a9a4d40b27123679bc3b43b8116c39690d4
Eduardo Francisco <eadwardus@disroot.org>
2023-09-29 12:02:18 -0300

    src/std/calloc.c: Fix initialization

--- src/std/calloc.c
+++ src/std/calloc.c
@@ -7,9 +7,6 @@ void *
 c_std_calloc(usize m, usize n)
 {
 	void *p;
-
-	if ((p = _tertium_allocator(nil, m, n)))
-		c_mem_set(p, n, 0);
-
+	if ((p = _tertium_allocator(nil, m, n))) c_mem_set(p, m * n, 0);
 	return p;
 }
