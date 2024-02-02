0fd41810a2432fd6e4d529e45835a055197e07b2
Eduardo Francisco <eadwardus@disroot.org>
2024-02-02 05:53:42 -0300

    rand/data.c: Fix pseudoentropy use

--- src/rand/data.c
+++ src/rand/data.c
@@ -4,9 +4,10 @@
 #define RFLAGS (_TERTIUM_SYS_GRNDNONBLOCK|_TERTIUM_SYS_GRNDRANDOM)
 
 static void
-pseudoentropy(char *s, usize n)
+pseudoentropy(char *s, usize off, usize n)
 {
 	ctype_hst hs;
+	ctype_rst r;
 	ctype_taia now;
 	usize i;
 	char seed[32];
@@ -18,13 +19,14 @@ pseudoentropy(char *s, usize n)
 	c_uint_32bigpack(seed+20, c_sys_getppid());
 	c_uint_64bigpack(seed+24, (uintptr)s);
 
-	/* extension */
+	/* datainc genseed */
 	c_hsh_siphash->init(&hs);
-	for (i = 0; i < n; i += 8) {
-		c_hsh_siphash->update(&hs, s, n);
-		c_hsh_siphash->update(&hs, seed, sizeof(seed));
-		c_hsh_siphash->end(&hs, (s + i) - (n - i));
-	}
+	c_hsh_siphash->update(&hs, s, n);
+	c_hsh_siphash->update(&hs, seed, sizeof(seed));
+	c_hsh_siphash->end(&hs, seed);
+	/* extension */
+	c_rand_setseed(&r, c_uint_64unpack(seed));
+	c_rand_datainc(&r, s + off, n - off);
 }
 
 char *
@@ -33,6 +35,6 @@ c_rand_data(char *s, usize n)
 	size r;
 	r = c_sys_getrandom(s, n, RFLAGS);
 	if (r < 0) r = 0;
-	if ((usize)r < n) pseudoentropy(s, n);
+	if ((usize)r < n) pseudoentropy(s, r, n);
 	return s;
 }
