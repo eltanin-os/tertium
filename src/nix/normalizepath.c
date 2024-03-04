6ff276e202b05a4d9a1b0cdfc62517c1688b4b2a
Eduardo Francisco <eadwardus@disroot.org>
2024-03-04 15:08:22 -0300

    nix/normalizepath.c: Fix for absolute /../

--- src/nix/normalizepath.c
+++ src/nix/normalizepath.c
@@ -9,7 +9,10 @@ c_nix_normalizepath(char *s, usize n)
 	char *prev;
 	char *tmp;
 
-	if (s[0] == '.' && s[1] == '/' && s[2]) c_str_cpy(s, n, s + 2);
+	if (s[0] == '.' && s[1] == '/' && s[2]) {
+		for (off = 2; s[off] == '/'; ++off) ;
+		c_str_cpy(s, n, s + off);
+	}
 	while ((tmp = c_str_str(s, n, "//"))) c_str_cpy(tmp, n, tmp + 1);
 	while ((tmp = c_str_str(s, n, "/./"))) c_str_cpy(tmp, n, tmp + 2);
 	/* strip "/../" only when redundant */
@@ -17,12 +20,18 @@ c_nix_normalizepath(char *s, usize n)
 		off = found = 0;
 		while ((tmp = c_str_str(s + off, n, "/../"))) {
 			diff = tmp - s;
-			off += diff;
-			prev = c_str_rchr(s, diff - 1, '/');
-			if (!prev) prev = s;
-			if (!C_STR_CMP("../", prev + (*prev == '/'))) continue;
-			diff = 3 + ((prev == s) && *s != '/');
-			c_str_cpy(prev, n, tmp + diff);
+			if (!(off += diff)) {
+				c_str_cpy(tmp + 1, n, tmp + 4);
+			} else {
+				if (!(prev = c_str_rchr(s, diff - 1, '/'))) {
+					prev = s;
+				}
+				if (!C_STR_CMP("../", prev + (*prev == '/'))) {
+					continue;
+				}
+				diff = 3;
+				c_str_cpy(prev, n, tmp + diff);
+			}
 			found = 1;
 			break;
 		}
